c8afd69b57407e35df1b2e8b9324340f40d84ee5
elyseeMB <elyseeMB@users.noreply.github.com>
2026-02-01 13:24:53 +0000

    Deploy main site: 5810acd3c90e60814a8aa53d9373d7cccdaae5ee elyseeMB/elyseemb.github.io@5810acd3c90e60814a8aa53d9373d7cccdaae5ee

---
+++ _astro/Footer.astro_astro_type_script_index_0_lang.Bsq0Z681.js
@@ -0,0 +1,1 @@
+import{o as e}from"./client.Dro3Yt0u.js";e(()=>{document.getElementById("footer-year").innerText=new Date().getFullYear()});
---
+++ _astro/client.Dro3Yt0u.js
@@ -0,0 +1,1 @@
+function e(t){return document.addEventListener("astro:page-load",t.bind(this))}export{e as o};
